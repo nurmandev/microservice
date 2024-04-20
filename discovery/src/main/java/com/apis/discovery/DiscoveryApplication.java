294c85d84946c54f20889b8ca607f0a2b1587bef
Adebayour66265 <adebayour66265@gmail.com>
2024-04-20 08:13:42 +0100

    add some endpoints

--- discovery/src/main/java/com/apis/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/apis/discovery/DiscoveryApplication.java
@@ -10,6 +10,7 @@ public class DiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
+		System.out.println("server connected to Discovery");
 	}
 
 }
